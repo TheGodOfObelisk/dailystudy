f6e79ac6797b236ff8191f4581282fd6f8d5d840
TheGodOfObelisk <1198582189@qq.com>
2018-12-02 15:19:09 +0800

    122

--- exe.cpp
+++ exe.cpp
@@ -1,52 +1,42 @@
 #include <iostream>
-#include <string>
+// #include <string>
+#include <vector>
+#include <math.h>
+#include <assert.h>
 
 using namespace std;
 
-string convert(string s, int numRows){
-    if(numRows == 1) return s;
-    if(numRows >= s.length()) return s;
-    char t[s.length() + 1] = {0};
-    int j = 0, index = 0;
-    // if(numRows == 2){
-    //     for(int i = 0; 2 * i < s.length(); i++)
-    //         t[index++] = s[2 * i];
-    //     for(int i = 0; (2 * i + 1) < s.length(); i++)
-    //         t[index++] = s[2 * i + 1];
-    // }
-    while(j < numRows){
-        if(j == 0){// the first line
-            for(int i = 0; 2*i * (numRows - 1) < s.length(); i++){
-                cout << "11 before assignment, the value of index is " << index << endl;
-                t[index++] = s[2 * i * (numRows - 1)];
-            }
-        }
-        else if(j == numRows - 1){// the last line
-            for(int i = 0; (2 * i + 1) * (numRows - 1) < s.length(); i++){
-                cout << "22 before assignment, the value of index is " << index << endl;
-                t[index++] = s[(2 * i + 1) * (numRows - 1)];
-            }
-        }
-        else{
-            for(int i = 0; (2 * i * (numRows -1) - j) < s.length() || (2 * i * (numRows -1) + j) < s.length(); i++){
-                if(((2 * i) * (numRows -1) - j) < s.length() && ((2 * i) * (numRows -1) - j) >= 0){
-                    cout << "33 before assignment, the value of index is " << index << endl;
-                    t[index++] = s[(2 * i) * (numRows -1) - j];
-                }
-                if(s[2 * i * (numRows -1) + j] != '\0'){
-                    cout << "44 before assignment, the value of index is " << index << endl;
-                    t[index++] = s[2 * i * (numRows -1) + j];
-                }
-            }
-        }
-        j++;
-    }
-    return string(t);
-}
+int reverse(int);
 
 int main(void){
-    string t = "ABCDEFGHIJKLMN";
-    string res = convert(t, 4);
-    cout << res << endl;
+    cout << reverse(-1563847412) << endl;;
 	return 0;
+}
+
+int reverse(int x){
+    vector<int> nList;
+    int res = 0;
+    bool isP;
+    int n = 0;
+    if(x == 0) return x;
+    if(x > 0) {
+        isP = true;
+    } else {
+        isP = false;
+        x = -x;
+    }
+    while(x > 0){
+        nList.push_back(x % 10);
+        x = x/10;
+        n++;
+    }
+    int k = 0;
+    while(k < n){
+        res = res + pow(10, k) * nList[n - 1 -k];
+        assert((res > 0 && isP) || (res < 0 && !isP));
+        k++;
+    }
+    if(isP && res < 0) return 0;
+    if(!isP) return -res;
+    return res;
 }
