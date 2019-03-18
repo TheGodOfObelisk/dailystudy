b36e65e150ff1193fdd0282e972e35c5f244ab5e
TheGodOfObelisk <1198582189@qq.com>
2019-03-18 19:15:36 +0800

    318

---
+++ execrise/exe1/main.cpp
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << MY_STRING << endl;
+    return 0;
+}
---
+++ execrise/exe1/main.h
@@ -0,0 +1,6 @@
+#ifndef MY_MAIN_H
+#define MY_MATH_H
+
+#define MY_STRING "hello world"
+
+#endif
